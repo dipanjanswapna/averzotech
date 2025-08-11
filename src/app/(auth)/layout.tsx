3519716833380a2f6d9d26333133730226cb4c7f
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-11 20:12:53 +0000

    login page stable koro o amon picture size  o amon system koro jate logi

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
+    <div className="w-full lg:grid min-h-screen lg:grid-cols-2">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
             <div className="grid gap-2 text-center">
@@ -18,13 +18,13 @@ export default function AuthLayout({
             {children}
         </div>
       </div>
-      <div className="hidden bg-muted lg:block">
+      <div className="hidden bg-muted lg:block relative">
         <Image
           src="https://placehold.co/1080x1920.png"
           alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          layout="fill"
+          objectFit="cover"
+          className="dark:brightness-[0.2] dark:grayscale"
           data-ai-hint="fashion advertisement"
         />
       </div>
