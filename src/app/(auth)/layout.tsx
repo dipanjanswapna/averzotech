c0202d33da7f74c6e12a78416f8ceb23539b5af6
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-11 20:14:24 +0000

    login form o picture er size soto koro jate scroll kora na lage

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,8 +9,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full lg:grid min-h-screen lg:grid-cols-2">
-      <div className="flex items-center justify-center py-12">
+    <div className="w-full lg:grid lg:grid-cols-2">
+      <div className="flex h-screen items-center justify-center">
         <div className="mx-auto grid w-[350px] gap-6">
             <div className="grid gap-2 text-center">
                  <Logo />
@@ -18,7 +18,7 @@ export default function AuthLayout({
             {children}
         </div>
       </div>
-      <div className="hidden bg-muted lg:block relative">
+      <div className="hidden bg-muted lg:block relative h-screen">
         <Image
           src="https://placehold.co/1080x1920.png"
           alt="Image"
