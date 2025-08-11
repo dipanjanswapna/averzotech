0ba60855bdc7f4040f241b1c2f7171e95f8eb582
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-11 06:26:58 +0000

    main nevigation bar aro chikon koro ba thin koro

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -137,7 +137,7 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-14 items-center">
         <div className="flex flex-1 items-center justify-between">
           <div className="flex items-center gap-6">
             <Link href="/">
