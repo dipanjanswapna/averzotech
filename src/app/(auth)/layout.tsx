95c1b27a3be27a998ad129cf00cbdcb0265f81af
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-11 20:19:22 +0000

    sign up form o picture er size soto koro jate scroll kora na lage then s

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,16 +9,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full lg:grid lg:grid-cols-2">
-      <div className="flex h-screen items-center justify-center">
-        <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-                 <Logo />
-            </div>
-            {children}
-        </div>
-      </div>
-      <div className="hidden bg-muted lg:block relative h-screen">
+    <div className="w-full lg:grid lg:grid-cols-2 min-h-screen">
+       <div className="hidden bg-muted lg:block relative h-screen order-first">
         <Image
           src="https://placehold.co/1080x1920.png"
           alt="Image"
@@ -27,6 +19,14 @@ export default function AuthLayout({
           className="dark:brightness-[0.2] dark:grayscale"
           data-ai-hint="fashion advertisement"
         />
+      </div>
+      <div className="flex h-screen items-center justify-center order-last">
+        <div className="mx-auto grid w-[350px] gap-6">
+            <div className="grid gap-2 text-center">
+                 <Logo />
+            </div>
+            {children}
+        </div>
       </div>
     </div>
   );
