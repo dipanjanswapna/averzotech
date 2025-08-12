186f3449322e1de3a2e25fb5541806c042c1b9fa
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-12 11:04:44 +0000

    build lo

--- src/components/logo.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export function Logo({ className }: { className?: string }) {
-  return (
-    <div className={cn("flex items-center", className)}>
-      <span className="text-2xl font-black tracking-wider">AVERZO</span>
-    </div>
-  );
-}
