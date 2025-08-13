30c4f372f95d3e7aab0cac8c7c21eb915486efac
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-13 19:35:33 +0000

    sobiti footer e onek soto koro dan pase obosthan korao

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -75,8 +75,8 @@ export function SiteFooter() {
                         <p className="text-sm text-muted-foreground">of receiving your order</p>
                     </div>
                 </div>
-                 <div className="mt-6">
-                   <Image src="https://i.postimg.cc/GH8FkRGD/ssl-commerce-1.png" width={280} height={60} alt="Payment methods" />
+                 <div className="mt-6 flex justify-end">
+                   <Image src="https://i.postimg.cc/GH8FkRGD/ssl-commerce-1.png" width={140} height={30} alt="Payment methods" />
                 </div>
             </div>
         </div>
