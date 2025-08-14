584a8be7fed593b49ee848511f2e77e3a412fcd1
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-14 17:53:25 +0000

    src folder er modde app er modhe logo.png ke amar web app er logo hisebe

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,11 @@
+
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center", className)}>
-      <span className="text-2xl font-black tracking-wider">AVERZO</span>
+      <Image src="/logo.png" alt="Averzo Logo" width={120} height={30} />
     </div>
   );
 }
