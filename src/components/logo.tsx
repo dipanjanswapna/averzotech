d4a428b3732285ebb70aeb1a3a9bfe7c1efaa10e
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-14 18:12:05 +0000

    same problem sho gora theke somossa ber kore somadhan koro

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,13 @@
 
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center", className)}>
-      <Image 
+      <img 
         src="/logo.png" 
         alt="Averzo Logo" 
-        width={120} 
-        height={30}
-        priority
+        style={{ height: '30px', width: 'auto' }}
       />
     </div>
   );
