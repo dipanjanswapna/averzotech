f30af18f037b3f864753c14dc655d0d2045bcf13
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-12-26 07:48:22 +0000

    import React from 'react'; import styled from 'styled-components';
    cons

--- src/components/ui/loading-spinner.tsx
+++ src/components/ui/loading-spinner.tsx
@@ -2,21 +2,19 @@
 'use client';
 
 import React from 'react';
+import { cn } from '@/lib/utils';
 
 export function LoadingSpinner() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden">
-        <div className="relative w-full flex justify-center">
-            <div className="animate-drive relative">
-                <div className="w-24 h-12 bg-blue-500 rounded-t-lg"></div>
-                <div className="w-12 h-10 bg-blue-400 absolute left-24 top-2 rounded-t-lg"></div>
-                <div className="absolute top-12 left-2 flex gap-12">
-                    <div className="w-8 h-8 bg-black rounded-full border-4 border-white animate-spin-wheel"></div>
-                    <div className="w-8 h-8 bg-black rounded-full border-4 border-white animate-spin-wheel"></div>
-                </div>
-            </div>
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden bg-background">
+        <div className="flex items-center justify-center space-x-2">
+            <div className="letter-a">A</div>
+            <div className="letter-v">V</div>
+            <div className="letter-e">E</div>
+            <div className="letter-r">R</div>
+            <div className="letter-z">Z</div>
+            <div className="letter-o">O</div>
         </div>
-        <p className="text-foreground mt-8 text-lg font-semibold">Loading...</p>
     </div>
   );
 }
