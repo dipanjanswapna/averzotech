2c76645b2a5337d41a9a9e2e624893d795b4c797
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-01-01 17:52:48 +0000

    env file update koro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,13 @@
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4q4m8pNjCB8ihd5jsU6AvgjZMp22WBAg",
-  authDomain: "averzo-marketplace.firebaseapp.com",
-  projectId: "averzo-marketplace",
-  storageBucket: "averzo-marketplace.appspot.com",
-  messagingSenderId: "237952506110",
-  appId: "1:237952506110:web:40ae8d76143c40523cadaf",
-  measurementId: "G-NVTRQ7EB2E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export { firebaseConfig };
